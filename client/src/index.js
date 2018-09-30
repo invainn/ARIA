66e9f7839a26f471d7f264cdbbcee58ff0b5e612
Anthony J Bennett <anthonybennett@nevada.unr.edu>
2018-09-30 04:38:40 -0700

    implemented theme, will expand later

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import theme from './theme';
 
 import './index.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import App from './containers/App/AppContainer';
 import registerServiceWorker from './registerServiceWorker';
+import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
     <Provider store={configureStore()}>
-        <App />
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
---
+++ client/src/theme.js
@@ -0,0 +1,19 @@
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import teal from '@material-ui/core/colors/teal';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: teal,
+        secondary: {
+            main: '#006064'
+        },
+        error: red,
+        
+        contrastThreshold: 3,
+
+        tonalOffset: 0.2,
+    },
+});
+
+export default theme;
