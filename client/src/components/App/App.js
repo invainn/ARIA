516b41287328b4b0502930e7cb2c9976b7270904
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2018-10-26 16:18:59 -0700

    Removed home page and changed Login screen to be the home page

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { withStyles } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from '../Home/Home';
 import Login from '../Login/Login';
 
 const styles = () => ({
@@ -16,8 +15,7 @@ const styles = () => ({
 const App = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
+            <Route exact path="/" component={Login} />
         </Switch>
     </BrowserRouter>
 );
