a9b760f1c9867e5b42b47d6e26e9f895de46ff86
Anthony J Bennett <anthonybennett@nevada.unr.edu>
2018-11-02 11:49:24 -0700

    Changed root URL component to direct to Login

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { withStyles } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NotFound from '../statuscodes/404/NotFound';
-import Home from '../Home/Home';
 import Dashboard from '../Shell/CustomerPortal/MenuOptions/Dashboard';
 import Account from '../Shell/CustomerPortal/MenuOptions/Account';
 import RegStudForm from '../Shell/CustomerPortal/MenuOptions/RegisteredStudentsForm/RegStudForm';
@@ -11,6 +10,7 @@ import FAQ from '../Shell/CustomerPortal/MenuOptions/FAQ';
 import AboutUs from '../Shell/CustomerPortal/MenuOptions/AboutUs';
 import EventsCalendar from '../Shell/CustomerPortal/MenuOptions/EventsCalendar';
 import EventRegistration from '../Shell/CustomerPortal/MenuOptions/EventRegistration';
+import Login from '../Login/Login';
 
 const styles = () => ({
     '@global body': {
@@ -23,7 +23,7 @@ const styles = () => ({
 const App = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Login} />
             <Route exact path="/customer" component={Dashboard} />
             <Route path="/customer/account-info" component={Account} />
             <Route exact path="/customer/registered-students" component={RegStudForm} />
