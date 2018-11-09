3d488c5e2d1692a34fa973a52ea7c934328de625
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2018-11-09 15:29:16 -0800

    Changed route from /customer/registered-students to /customer/participants

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -10,6 +10,8 @@ import FAQ from '../Shell/CustomerPortal/MenuOptions/FAQ';
 import AboutUs from '../Shell/CustomerPortal/MenuOptions/AboutUs';
 import EventsCalendar from '../Shell/CustomerPortal/MenuOptions/EventsCalendar';
 import EventRegistration from '../Shell/CustomerPortal/MenuOptions/EventRegistration';
+import ActiveRegistration from '../Shell/CustomerPortal/MenuOptions/ActiveRegistration/ActiveRegistrations';
+// import CustomerPortalContainer from '../../containers/Shell/CustomerPortal/CustomerPortalContainer';
 import Login from '../Login/Login';
 
 const styles = () => ({
@@ -26,13 +28,13 @@ const App = () => (
             <Route exact path="/" component={Login} />
             <Route exact path="/customer" component={Dashboard} />
             <Route path="/customer/account-info" component={Account} />
-            <Route exact path="/customer/registered-students" component={RegStudForm} />
+            <Route exact path="/customer/participants" component={RegStudForm} />
             <Route exact path="/customer/events-calendar" component={EventsCalendar} />
             <Route exact path="/customer/register-for-an-event" component={EventRegistration} />
             <Route exact path="/customer/faq" component={FAQ} />
             <Route exact path="/customer/about" component={AboutUs} />
-    {/*        <Route exact path="/customer/active-registrations" render={props => <CustomerPortalContainer {...props} pathname={props.location} />} />
-            <Route exact path="/customer/payment-history" render={props => <CustomerPortalContainer {...props} pathname={props.location} />} />
+            <Route exact path="/customer/active-registrations" component={ActiveRegistration} />
+    {/*        <Route exact path="/customer/payment-history" render={props => <CustomerPortalContainer {...props} pathname={props.location} />} />
             <Route exact path="/customer/faq" render={props => <CustomerPortalContainer {...props} pathname={props.location} />} />
             <Route exact path="/customer/about" render={props => <CustomerPortalContainer {...props} pathname={props.location} />} /> */}
             <Route path="*" component={NotFound} />
