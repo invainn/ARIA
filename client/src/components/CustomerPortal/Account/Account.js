8cff29b6cdcf7922617f163d58d64eea4367fb7e
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2018-11-16 15:25:35 -0800

    Resolved conflict.

--- client/src/components/CustomerPortal/Account/Account.js
+++ client/src/components/CustomerPortal/Account/Account.js
@@ -6,9 +6,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { 
-Icon,
-FormHelperText,
+import {
+  Icon,
 } from '@material-ui/core';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import AccountFields from './AccountFields/AccountFields';
