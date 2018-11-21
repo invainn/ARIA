4f658013d5fe9617eb7ccdc5c6d73ef0eb635569
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2018-11-21 15:23:22 -0800

    Created AccountCreation page

---
+++ client/src/components/AccountCreation/AccountCreation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+    Grid,
+    Typography,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import Shell from '../Shell/Shell';
+
+const styles = theme => ({
+    root: {
+
+    },
+
+    landingContent: {
+        background: 'rgba(55, 71, 79, 0.3)',
+    },
+
+    margin: {
+        margin: theme.spacing.unit,
+    },
+});
+
+const AccountCreation = ({ classes }) => (
+    <Shell>
+        <Grid className={classes.root}>
+            <Typography>
+                Test
+            </Typography>
+        </Grid>
+    </Shell>
+);
+
+export default withStyles(styles)(AccountCreation)
