2c5c83d081c77ab98f0623cd774b571cf90cebeb
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2019-01-30 16:29:06 -0800

    Changed buttons to checkboxes and added a stepper for progression

--- client/src/components/CustomerPortal/AdminPortal/GenerateDocuments/GenerateDocuments.js
+++ client/src/components/CustomerPortal/AdminPortal/GenerateDocuments/GenerateDocuments.js
@@ -1,18 +1,20 @@
+// TODO: FIX NEXT BUTTON WORK CORRECTLY
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import {
   Button,
-  Paper,
   Typography,
-  Radio,
-  RadioGroup,
+  Grid,
+  Checkbox,
   FormControlLabel,
-  FormControl,
-  FormLabel,
-  ListItemIcon,
-  Icon,
+  FormGroup,
+  Stepper,
+  Step,
+  StepLabel,
 } from '@material-ui/core';
 import CustomerPortalContainer from '../../../../containers/Shell/CustomerPortal/CustomerPortalContainer';
+
+import SelectFile from './SelectFile';
 
 const styles = theme => ({
   root: {
@@ -33,19 +35,55 @@ const styles = theme => ({
   group: {
     margin: '$(theme.spacing.unit)px 0',
   },
+  documentStepper: {
+    borderRadius: '5px',
+  },
 });
 
 class GenerateDocuments extends React.Component {
   state = {
-    value: 'Adjudication Forms',
+      adjudication: false,
+      results: false,
+      announcing: false,
+      teacherMaster: false,
+      sessionAssignments: false,
+      room: false,
+      master: false,
+      simplified: false,
+      teacherList: false,
+      theory: false,
+      sessionFiles: false,
+      activeStep: 0,
   };
 
-  handleChange = ({ event }) => {
-    this.setState({ value: event.target.value });
+  handleChange = form => ({ event }) => {
+    this.setState({ [form]: event.target.checked });
   };
+
+  walkStep() {
+    this.setState(({ activeStep }) => ({ activeStep: activeStep + 1 }));
+  }
+
+  walkBackStep() {
+    this.setState(({ activeStep }) => ({ activeStep: activeStep - 1 }));
+}
 
   render() {
     const { classes } = this.props;
+    const {
+      adjudication,
+      results,
+      announcing,
+      teacherMaster,
+      sessionAssignments,
+      room,
+      master,
+      simplified,
+      teacherList,
+      theory,
+      sessionFiles,
+      activeStep,
+    } = this.state;
 
     return (
       <CustomerPortalContainer userType={0}>
@@ -54,102 +92,105 @@ class GenerateDocuments extends React.Component {
               Document Generation Configurations
           </Typography>
         </div>
-        <Paper className={classes.root}>
-          <Typography variant="h6" align="center">
-              Please select which documents to download:
-          </Typography>
-        </Paper>
-        <div>
-            <FormControl component="fieldset" className={classes.documentPaper}>
-                <FormLabel component="legend">Documents</FormLabel>
-                <Button size="small">
-                Select All
-                </Button>
-                <RadioGroup
-                  aria-label="documents"
-                  name="documents"
-                  className={classes.group}
-                  // eslint-disable-next-line react/destructuring-assignment
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                >
-                    <FormControlLabel
-                      value="Adjudication Forms"
-                      control={<Radio color="primary" />}
-                      label="Adjudication Forms"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Results Sheets"
-                      control={<Radio color="primary" />}
-                      label="Results Sheets"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Announcing Sheets"
-                      control={<Radio color="primary" />}
-                      label="Announcing Sheets"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Teacher Master"
-                      control={<Radio color="primary" />}
-                      label="Teacher Master"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Session Assignments"
-                      control={<Radio color="primary" />}
-                      label="Session Assignments"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Room Schedules"
-                      control={<Radio color="primary" />}
-                      label="Room Schedules"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Master Schedule"
-                      control={<Radio color="primary" />}
-                      label="Master Schedule"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Simplified Schedule"
-                      control={<Radio color="primary" />}
-                      label="Simplified Schedule"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Teacher List"
-                      control={<Radio color="primary" />}
-                      label="Teacher List"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Theory Scores"
-                      control={<Radio color="primary" />}
-                      label="Theory Scores"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value="Session Files"
-                      control={<Radio color="primary" />}
-                      label="Session Files"
-                      labelPlacement="start"
-                    />
-                </RadioGroup>
-            </FormControl>
-        </div>
-        <div>
-            <Button variant="contained" className={classes.button} color="primary">
-                <ListItemIcon>
-                    <Icon>save</Icon>
-                </ListItemIcon>
-                    Save Changes
+        <Stepper activeStep={activeStep} className={classes.documentStepper}>
+        {[
+          'Select Documents',
+          'Select File Type',
+          'Generate Documents',
+        ].map(item => (
+          <Step>
+            <StepLabel>{item}</StepLabel>
+          </Step>
+        ))}
+        </Stepper>
+        <Grid container direction="column" justify="center" alignItems="flex-end">
+          <FormGroup>
+            <Grid xs={7}>
+              <FormControlLabel
+                control={
+                  <Checkbox checked={adjudication} onChange={this.handleChange('adjudication')} value="adjudication" />
+                }
+                label="Adjudication Forms"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={results} onChange={this.handleChange('results')} value="results" />
+                }
+                label="Results Sheets"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={announcing} onChange={this.handleChange('announcing')} value="announcing" />
+                }
+                label="Announcing Sheets"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={teacherMaster} onChange={this.handleChange('teacherMaster')} value="teacherMaster" />
+                }
+                label="Teacher Master"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={sessionAssignments} onChange={this.handleChange('sessionAssignments')} value="sessionAssignments" />
+                }
+                label="Session Assignments"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={room} onChange={this.handleChange('room')} value="room" />
+                }
+                label="Room Schedules"
+              />
+            </Grid>
+            <Grid xs={7}>
+              <FormControlLabel
+                control={
+                  <Checkbox checked={master} onChange={this.handleChange('master')} value="master" />
+                }
+                label="Master Schedule"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={simplified} onChange={this.handleChange('simplified')} value="simplified" />
+                }
+                label="Simplified Schedule"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={teacherList} onChange={this.handleChange('teacherList')} value="teacherList" />
+                }
+                label="Teacher List"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={theory} onChange={this.handleChange('theory')} value="theory" />
+                }
+                label="Theory Scores"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={sessionFiles} onChange={this.handleChange('sessionFiles')} value="sessionFiles" />
+                }
+                label="Session Files"
+              />
+            </Grid>
+          </FormGroup>
+        </Grid>
+        <Grid item direction="column" justify="center" alignItems="center">
+            <Grid xs={8}>
+            { activeStep === 1 && <SelectFile /> }
+            { /* { activeStep === 1 && <DownloadFiles /> } */}
+            { activeStep > 0 && (
+              <Button variant="contained" className={classes.button} color="primary" onClick={this.walkBackStep}>
+                Go Back
+              </Button>
+            )}
+            <Button variant="contained" className={classes.button} onClick={this.walkStep}>
+              Next
             </Button>
-        </div>
+            </Grid>
+        </Grid>
       </CustomerPortalContainer>
     );
   }
