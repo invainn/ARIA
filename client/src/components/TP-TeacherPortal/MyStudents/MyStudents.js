5003e70b8edd3e3c8647859237f2c54f96deddcd
Anthony J Bennett <anthonybennett@nevada.unr.edu>
2019-05-03 07:37:40 -0700

    Events and Students now propagate to corresponding teachers

--- client/src/components/TP-TeacherPortal/MyStudents/MyStudents.js
+++ client/src/components/TP-TeacherPortal/MyStudents/MyStudents.js
@@ -1,303 +1,99 @@
-import React from 'react';
-import classNames from 'classnames';
+/* eslint-disable camelcase */
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TablePagination,
-  TableRow,
-  Toolbar,
-  Typography,
   Paper,
-  Checkbox,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  Table,
+  Grid,
+  Typography,
+  CircularProgress,
 } from '@material-ui/core';
-import SchoolIcon from '@material-ui/icons/School';
-import { lighten } from '@material-ui/core/styles/colorManipulator';
-import EnhancedTableHead from './EnhancedTableHead';
+
 import CustomerPortalContainer from '../../../containers/Shell/CustomerPortalContainer/CustomerPortalContainer';
 
-// Do not do this, fix this
-let counter = 0;
+import ARIA_SERVER_URL from '../../../config';
 
 const styles = theme => ({
   root: {
-    width: '100%',
-  },
-  table: {
-    minWidth: 1020,
-  },
-  tableWrapper: {
+    width: '45vw',
+    marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
   },
   pageTitle: {
-    width: '100%',
-    textDecoration: 'underline',
-    textDecorationColor: (theme.palette.type === 'light') ? '#000000' : '#FFFFFF',
-    paddingBottom: '15px',
+      width: '100%',
+      textDecoration: 'underline',
+      textDecorationColor: (theme.palette.type === 'light') ? '#000000' : '#FFFFFF',
+      paddingBottom: '15px',
+  },
+  button: {
+    margin: '10px',
   },
 });
 
-const createData = (firstName, middleInitial, lastName, suffix, musicLevel) => {
-    counter += 1;
-    return {
-        id: counter, firstName, middleInitial, lastName, suffix, musicLevel,
-    };
-};
-
-const desc = (a, b, orderBy) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-
-  return 0;
-};
-
-function stableSort(array, cmp) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-}
-
-function getSorting(order, orderBy) {
-  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
-}
-
-const toolbarStyles = theme => ({
-  root: {
-    paddingRight: theme.spacing.unit,
-    color: theme.palette.text.primary,
-    backgroundColor: theme.palette.primary,
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.primary.main,
-          backgroundColor: lighten(theme.palette.primary.main, 0.75),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.primary,
-        },
-  spacer: {
-    flex: '1 1 100%',
-  },
-  actions: {
-    color: theme.palette.text.primary,
-  },
-  title: {
-    flex: '0 0 auto',
-  },
-});
-
-let EnhancedTableToolbar = (props) => {
-  const { numSelected, classes } = props;
-
-  return (
-    <Toolbar
-      className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      <div className={classes.title}>
-        {numSelected > 0 ? (
-          <Typography color="inherit" variant="h6">
-            {numSelected} selected
-          </Typography>
-        ) : (
-          <SchoolIcon />
-        )}
-      </div>
-      <div className={classes.spacer} />
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
-
-/* const styles = () => ({
-  root: {
-    width: '100%',
-  },
-  table: {
-    minWidth: 1020,
-  },
-  tableWrapper: {
-    overflowX: 'auto',
-  },
-}); */
-
-class EnhancedTable extends React.Component {
+class MyStudents extends Component {
   state = {
-    order: 'asc',
-    orderBy: 'First Name',
-    selected: [],
-    // TODO: Create a data file instead of hard coding inside of code for future use
-    data: [
-      createData('Alice', 'P', 'Smith', 'Jr', 2),
-      createData('Mary', 'B', 'Daniels', 'Sr', 11),
-      createData('Ronald', 'E', 'Davidson', '-', 4),
-      createData('Scott', 'K', 'Brown', '-', 6),
-      createData('Raymond', 'I', 'McMann', 'Jr', 1),
-      createData('Kenneth', 'B', 'Honeycomb', '-', 8),
-      createData('Gary', 'N', 'Peters', 'Sr', 3),
-      createData('Joshua', 'S', 'Holyfield', '-', 9),
-      createData('Heather', 'D', 'Howard', '-', 6),
-      createData('Lou', 'V', 'York', '-', 8),
-      createData('Jack', 'S', 'Ybarra', '-', 1),
-      createData('Steve', 'A', 'Noack', 'Jr', 10),
-      createData('Gabriella', 'I', 'Barnett', 'Jr', 6),
-    ],
-    page: 0,
-    rowsPerPage: 5,
+    students: [],
   };
 
-  handleRequestSort = (event, property) => {
-    let { order = 'desc' } = this.state;
-    const { orderBy = property } = this.state;
+  componentWillMount = async () => {
+    // Fetch participants related to Customer
+    const req = await axios.get(`${ARIA_SERVER_URL}/teacher/student`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwtToken')}`,
+      },
+    });
 
-    if (orderBy === property && order === 'desc') {
-      order = 'asc';
-    }
-
-    this.setState({ order, orderBy });
-  };
-
-  handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      this.setState(state => ({ selected: state.data.map(n => n.id) }));
-      return;
-    }
-    this.setState({ selected: [] });
-  };
-
-  handleClick = (event, id) => {
-    const { selected } = this.state;
-    const selectedIndex = selected.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    this.setState({ selected: newSelected });
-  };
-
-  handleChangePage = (event, page) => {
-    this.setState({ page });
-  };
-
-  handleChangeRowsPerPage = (event) => {
-    this.setState({ rowsPerPage: event.target.value });
-  };
-
-  isSelected = (id) => {
-    const { selected } = this.state;
-    return selected.indexOf(id) !== -1;
+    this.setState({ students: [...req.data] });
   };
 
   render() {
+    const { students } = this.state;
     const { classes } = this.props;
-    const {
-        data,
-        order,
-        orderBy,
-        selected,
-        rowsPerPage,
-        page,
-    } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
     return (
-        <CustomerPortalContainer userType={1}>
-          <div className={classes.pageTitle}>
+      <CustomerPortalContainer userType={1}>
+        <div className={classes.pageTitle}>
             <Typography component="h2" variant="h2" gutterBottom align="center">
               My Students
             </Typography>
-          </div>
-            <Paper className={classes.root}>
-                <EnhancedTableToolbar numSelected={selected.length} />
-                <div className={classes.tableWrapper}>
-                <Table className={classes.table} aria-labelledby="tableTitle">
-                    <EnhancedTableHead
-                      numSelected={selected.length}
-                      order={order}
-                      orderBy={orderBy}
-                      onSelectAllClick={this.handleSelectAllClick}
-                      onRequestSort={this.handleRequestSort}
-                      rowCount={data.length}
-                    />
-                    <TableBody>
-                    {stableSort(data, getSorting(order, orderBy))
-                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        .map((n) => {
-                        const isSelected = this.isSelected(n.id);
-                        return (
-                            <TableRow
-                              hover
-                              onClick={event => this.handleClick(event, n.id)}
-                              role="checkbox"
-                              aria-checked={isSelected}
-                              tabIndex={-1}
-                              key={n.id}
-                              selected={isSelected}
-                            >
-                            <TableCell padding="checkbox">
-                                <Checkbox color="primary" checked={isSelected} />
-                            </TableCell>
-                            <TableCell component="th" scope="row" padding="none">
-                                {n.firstName}
-                            </TableCell>
-                            <TableCell>{n.middleInitial}</TableCell>
-                            <TableCell>{n.lastName}</TableCell>
-                            <TableCell>{n.suffix}</TableCell>
-                            <TableCell>{n.musicLevel}</TableCell>
-                            </TableRow>
-                        );
-                        })}
-                    {emptyRows > 0 && (
-                        <TableRow style={{ height: 49 * emptyRows }}>
-                        <TableCell colSpan={5} />
-                        </TableRow>
-                    )}
-                    </TableBody>
-                </Table>
-                </div>
-                <TablePagination
-                  component="div"
-                  count={data.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  backIconButtonProps={{
-                    'aria-label': 'Previous Page',
-                }}
-                  nextIconButtonProps={{
-                    'aria-label': 'Next Page',
-                }}
-                  onChangePage={this.handleChangePage}
-                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                />
-            </Paper>
-        </CustomerPortalContainer>
+        </div>
+        <Grid container justify="center" alignItems="center">
+          <Paper className={classes.root}>
+            { (students.length === 0) ? (
+              <Grid container justify="center" alignItems="center">
+                <CircularProgress color="primary" />
+              </Grid>
+            ) : (
+              <Table className={classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">First Name</TableCell>
+                    <TableCell align="left">Last Name</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {students.map(({
+                    FirstName,
+                    LastName,
+                  }) => (
+                    <TableRow key={FirstName}>
+                      <TableCell align="left">{FirstName}</TableCell>
+                      <TableCell align="left">{LastName}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
+          </Paper>
+        </Grid>
+      </CustomerPortalContainer>
     );
   }
 }
 
-export default withStyles(styles)(EnhancedTable);
+export default withStyles(styles)(MyStudents);
--- client/src/components/TP-TeacherPortal/StudentRegistrations/StudentRegistrations.js
+++ client/src/components/TP-TeacherPortal/StudentRegistrations/StudentRegistrations.js
@@ -1,293 +1,102 @@
-/* eslint-disable max-len */
+/* eslint-disable camelcase */
 import React, { Component } from 'react';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TablePagination,
-  TableRow,
-  Toolbar,
-  Typography,
   Paper,
-  Checkbox,
-} from '@material-ui/core/';
-import SchoolIcon from '@material-ui/icons/School';
-import EnhancedTableHead from './EnhancedTableHead';
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  Table,
+  Grid,
+  Typography,
+  CircularProgress,
+} from '@material-ui/core';
+
 import CustomerPortalContainer from '../../../containers/Shell/CustomerPortalContainer/CustomerPortalContainer';
+
+import ARIA_SERVER_URL from '../../../config';
 
 const styles = theme => ({
   root: {
-    width: '100%',
-  },
-  table: {
-    minWidth: 1020,
-  },
-  tableWrapper: {
+    width: '45vw',
+    marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
   },
   pageTitle: {
-    width: '100%',
-    textDecoration: 'underline',
-    textDecorationColor: (theme.palette.type === 'light') ? '#000000' : '#FFFFFF',
-    paddingBottom: '15px',
+      width: '100%',
+      textDecoration: 'underline',
+      textDecorationColor: (theme.palette.type === 'light') ? '#000000' : '#FFFFFF',
+      paddingBottom: '15px',
   },
   button: {
-    margin: theme.spacing.unit,
+    margin: '10px',
   },
 });
 
-function createData(firstName, lastName, suffix, eventType, date, performanceTime, location, startTime,
-    endTime, commandPerformance, song1, song2, song3) {
-    return {
-        firstName,
-        lastName,
-        suffix,
-        eventType,
-        date,
-        performanceTime,
-        location,
-        startTime,
-        endTime,
-        commandPerformance,
-        song1,
-        song2,
-        song3,
-    };
-}
-
-const desc = (a, b, orderBy) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-
-  return 0;
-};
-
-// TODO: Javascript already has a sort, do not do this
-const stableSort = (array, cmp) => {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-
-  return stabilizedThis.map(el => el[0]);
-};
-
-const getSorting = (order, orderBy) => (order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy));
-
-let EnhancedTableToolbar = (props) => {
-  const { numSelected, classes } = props;
-
-  return (
-    <Toolbar
-      className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      <div className={classes.title}>
-        {numSelected > 0 ? (
-          <Typography color="inherit" variant="h6">
-            {numSelected} selected
-          </Typography>
-        ) : (
-          <SchoolIcon />
-        )}
-      </div>
-      <div className={classes.spacer} />
-    </Toolbar>
-  );
-};
-
-
-EnhancedTableToolbar = withStyles(styles)(EnhancedTableToolbar);
-
 class StudentRegistrations extends Component {
   state = {
-    order: 'asc',
-    orderBy: 'firstName',
-    selected: [],
-    // TODO: Create a data file instead of hard coding inside of code for future use
-    data: [
-      createData('Alice', 'Smith', 'Jr', 'Halloween Recital', '10/15/18', '6:00 PM', 'DMS 103', '5:00 AM', '9:00 PM', true, 'Ludwig Van Beethoven', 'Chopin', 'Help'),
-      createData('Bob', 'Honeycomb', 'Sr', 'Halloween Recital', '10/15/18', '6:00 PM', 'DMS 103', '5:00 AM', '9:00 PM', true, 'Ludwig Van Beethoven', 'Chopin', 'Help'),
-      createData('Jack', 'Reynolds', '-', 'Halloween Recital', '10/15/18', '6:00 PM', 'DMS 103', '5:00 AM', '9:00 PM', true, 'Ludwig Van Beethoven', 'Chopin', 'Help'),
-    ],
-    page: 0,
-    rowsPerPage: 5,
+    students: [],
   };
 
-  handleRequestSort = (event, property) => {
-    let { order = 'desc' } = this.state;
-    const { orderBy = property } = this.state;
+  componentWillMount = async () => {
+    // Fetch participants related to Customer
+    const req = await axios.get(`${ARIA_SERVER_URL}/teacher/student/registered`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwtToken')}`,
+      },
+    });
 
-    if (orderBy === property && order === 'desc') {
-      order = 'asc';
-    }
-
-    this.setState({ order, orderBy });
-  };
-
-  handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      this.setState(state => ({ selected: state.data.map(n => n.id) }));
-      return;
-    }
-    this.setState({ selected: [] });
-  };
-
-  handleClick = (event, id) => {
-    const { selected } = this.state;
-    const selectedIndex = selected.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    this.setState({ selected: newSelected });
-  }
-
-  handleChangePage = (event, page) => {
-    this.setState({ page });
-  };
-
-  handleChangeRowsPerPage = (event) => {
-    this.setState({ rowsPerPage: event.target.value });
-  };
-
-  isSelected = (id) => {
-    const { selected } = this.state;
-    return selected.indexOf(id) !== -1;
+    this.setState({ students: [...req.data] });
   };
 
   render() {
+    const { students } = this.state;
     const { classes } = this.props;
-    const {
-        data,
-        order,
-        orderBy,
-        selected,
-        rowsPerPage,
-        page,
-    } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
     return (
-        <CustomerPortalContainer userType={1}>
-          <div className={classes.pageTitle}>
-              <Typography component="h2" variant="h2" gutterBottom align="center">
-                Student Registrations
-              </Typography>
-          </div>
-            <Paper className={classes.root}>
-                <EnhancedTableToolbar numSelected={selected.length} />
-                <div className={classes.tableWrapper}>
-                <Table className={classes.table} aria-labelledby="tableTitle">
-                    <EnhancedTableHead
-                      numSelected={selected.length}
-                      order={order}
-                      orderBy={orderBy}
-                      onSelectAllClick={this.handleSelectAllClick}
-                      onRequestSort={this.handleRequestSort}
-                      rowCount={data.length}
-                    />
-                    <TableBody>
-                    {stableSort(data, getSorting(order, orderBy))
-                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        .map((n) => {
-                        const isSelected = this.isSelected(n.id);
-
-                        return [
-                          <TableRow
-                            hover
-                            onClick={event => this.handleClick(event, n.id)}
-                            role="checkbox"
-                            aria-checked={isSelected}
-                            tabIndex={-1}
-                            key={n.id}
-                            selected={isSelected}
-                            padding="auto"
-                          >
-                              <TableCell padding="checkbox">
-                                <Checkbox color="primary" checked={isSelected} />
-                              </TableCell>
-                              <TableCell>{n.firstName}</TableCell>
-                              <TableCell>{n.lastName}</TableCell>
-                              <TableCell>{n.suffix}</TableCell>
-                              <TableCell>{n.eventType}</TableCell>
-                              <TableCell>{n.date}</TableCell>
-                              <TableCell>{n.performanceTime}</TableCell>
-                              <TableCell>{n.location}</TableCell>
-                          </TableRow>,
-                          <TableRow
-                            style={{ display: isSelected ? undefined : 'none' }}
-                            hover
-                            onClick={event => this.handleClick(event, n.id)}
-                            role="checkbox"
-                            aria-checked={isSelected}
-                            tabIndex={-1}
-                            key={n.id}
-                            selected={isSelected}
-                            padding="auto"
-                          >
-                          {/* Stuff was broken new branch */}
-                            <TableCell />
-                            <TableCell />
-                            <TableCell>Song 1: {n.song1}</TableCell>
-                            <TableCell>Song 2: {n.song2}</TableCell>
-                            <TableCell>Song 3: {n.song3}</TableCell>
-                            <TableCell>Command Performance: {n.commandPerformance}</TableCell>
-                            <TableCell>Start Time: {n.startTime}</TableCell>
-                            <TableCell>End Time: {n.endTime}</TableCell>
-                            <TableCell />
-                          </TableRow>,
-                        ];
-                        })}
-                    {emptyRows > 0 && (
-                        <TableRow style={{ height: 49 * emptyRows }}>
-                        <TableCell colSpan={7} />
-                        </TableRow>
-                    )}
-                    </TableBody>
-                </Table>
-                </div>
-                <TablePagination
-                  component="div"
-                  count={data.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  backIconButtonProps={{
-                    'aria-label': 'Previous Page',
-                }}
-                  nextIconButtonProps={{
-                    'aria-label': 'Next Page',
-                }}
-                  onChangePage={this.handleChangePage}
-                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                />
-            </Paper>
-        </CustomerPortalContainer>
+      <CustomerPortalContainer userType={1}>
+        <div className={classes.pageTitle}>
+            <Typography component="h2" variant="h2" gutterBottom align="center">
+              Student Registrations
+            </Typography>
+        </div>
+        <Grid container justify="center" alignItems="center">
+          <Paper className={classes.root}>
+            { (students.length === 0) ? (
+              <Grid container justify="center" alignItems="center">
+                <CircularProgress color="primary" />
+              </Grid>
+            ) : (
+              <Table className={classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">First Name</TableCell>
+                    <TableCell align="left">Last Name</TableCell>
+                    <TableCell align="right">Event Name</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {students.map(({
+                    FirstName,
+                    LastName,
+                    EventName,
+                  }) => (
+                    <TableRow key={FirstName}>
+                      <TableCell align="left">{FirstName}</TableCell>
+                      <TableCell align="left">{LastName}</TableCell>
+                      <TableCell align="right">{EventName}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
+          </Paper>
+        </Grid>
+      </CustomerPortalContainer>
     );
   }
 }
-
 
 export default withStyles(styles)(StudentRegistrations);
